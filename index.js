7b5fe81b59af3a26a4589604d4ab034a4b3b8cf1
GabrielMM182 <gmm_morais@hotmail.com>
2022-04-26 10:46:33 -0300

    test 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const port = 4200
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
     res.send('ola heroku deu certo !!');
